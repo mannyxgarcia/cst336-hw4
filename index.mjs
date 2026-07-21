1db41fc16af5f6627d5eb9dc92b82f045fe837d3
Manny Garcia <mannyalgarcia@gmail.com>
2026-07-21 14:28:41 -0700

    Added routes and updates connection.ejs

--- index.mjs
+++ index.mjs
@@ -4,7 +4,33 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    currentPage: 'home',
+  });
+});
+
+app.get('/connection', (req, res) => {
+  res.render('connection', {
+    currentPage: 'connection',
+  });
+});
+
+app.get('/websites', (req, res) => {
+  res.render('websites', {
+    currentPage: 'websites',
+  });
+});
+
+app.get('/modern', (req, res) => {
+  res.render('modern', {
+    currentPage: 'modern',
+  });
+});
+
+app.get('/sources', (req, res) => {
+  res.render('sources', {
+    currentPage: 'sources',
+  });
 });
 
 const PORT = process.env.PORT || 3000;
