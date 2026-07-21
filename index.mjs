704659b0473b15ef1db775117d3fddbd9edc1fe9
Manny Garcia <mannyalgarcia@gmail.com>
2026-07-21 13:30:09 -0700

    Initial Express setup

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started!`);
+  console.log(`http://localhost:${PORT}`);
+});
